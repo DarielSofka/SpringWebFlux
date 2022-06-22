899509984887449962d2c65cb08de4dc1d6bd400
DarielSofka <dairel.desosa@sofka.com.co>
2022-06-22 14:33:55 -0300

    Rutas Reactivas Finalizado CRUD

--- apiWebFlux/src/main/java/com/example/apiWebFlux/Handler/ProductorHandler.java
+++ apiWebFlux/src/main/java/com/example/apiWebFlux/Handler/ProductorHandler.java
@@ -2,14 +2,17 @@ package com.example.apiWebFlux.Handler;
 
 import com.example.apiWebFlux.Services.ProductoService;
 import com.example.apiWebFlux.models.Producto;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.web.reactive.function.BodyInserters.*;
+
+import java.net.URI;
+import java.util.Date;
+
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -17,10 +20,67 @@ public class ProductorHandler {
     @Autowired
     private ProductoService service;
 
-    public Mono<ServerResponse> listar(ServerRequest request){
+    //Listar todos los Productos----------------------------
+    public Mono<ServerResponse> listar(ServerRequest request) {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .body(service.findAll(), Producto.class);
+    }
+
+    //Buscar Producto por ID-----------------------------------
+    public Mono<ServerResponse> ver(ServerRequest request) {
+
+        String id = request.pathVariable("id");
+        return service.findById(id).flatMap(p -> ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .body(fromObject(p)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    //Crear un Producto---------------------------------------
+    public Mono<ServerResponse> crear(ServerRequest request){
+        Mono<Producto> producto = request.bodyToMono(Producto.class);
+
+        return producto.flatMap(p -> {
+            if(p.getCreateAt() ==null) {
+                p.setCreateAt(new Date());
+            }
+            return service.save(p);
+        }).flatMap(p -> ServerResponse
+                .created(URI.create("/api/v2/productos/".concat(p.getId())))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(fromObject(p)));
+    }
+
+    //Actualizar Producto------------------------------------
+    public Mono<ServerResponse> editar(ServerRequest request){
+        Mono<Producto> producto = request.bodyToMono(Producto.class);
+        String id = request.pathVariable("id");
+
+        Mono<Producto> productoDb = service.findById(id);
+
+        return productoDb.zipWith(producto, (db, req) ->{
+                    db.setNombre(req.getNombre());
+                    db.setPrecio(req.getPrecio());
+                    db.setCategoria(req.getCategoria());
+                    return db;
+                }).flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .body(service.save(p), Producto.class))
+                .switchIfEmpty(ServerResponse.notFound().build());
+
+    }
+
+    //Eliminar Producto------------------------------
+    public Mono<ServerResponse> eliminar(ServerRequest request){
+        String id = request.pathVariable("id");
+
+        Mono<Producto> productoDb = service.findById(id);
+
+        return productoDb.flatMap(p-> service.delete(p).then(ServerResponse.noContent().build()))
+                .switchIfEmpty(ServerResponse.notFound().build());
+
     }
 
 }
--- apiWebFlux/src/main/java/com/example/apiWebFlux/Router/RouterFunctionConfig.java
+++ apiWebFlux/src/main/java/com/example/apiWebFlux/Router/RouterFunctionConfig.java
@@ -1,26 +1,26 @@
 package com.example.apiWebFlux.Router;
 
 import com.example.apiWebFlux.Handler.ProductorHandler;
-import com.example.apiWebFlux.Services.ProductoService;
-import com.example.apiWebFlux.models.Producto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 public class RouterFunctionConfig {
 
     @Bean
     public RouterFunction<ServerResponse> routes(ProductorHandler handler){
-        return route(GET("/api/v2").or(GET("/api/v3")), request -> handler.listar(request));
+        return route(GET("/api/v2/productos").or(GET("/api/v3/productos")), handler::listar)
+                .andRoute(GET("/api/v2/productos/{id}"), handler::ver)
+                .andRoute(POST("/api/v2/productos"), handler::crear)
+                .andRoute(PUT("/api/v2/productos/{id}"), handler::editar)
+                .andRoute(DELETE("/api/v2/productos/{id}"), handler::eliminar);
     }
 
 }
